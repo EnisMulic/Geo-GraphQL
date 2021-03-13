be51b8123e8d66707fce285b8027e6ed410c9d24
Enis Mulić <enis.s.mulic@gmail.com>
2021-03-13 17:14:16 +0100

    Add update country payload

--- Geo.Contracts/Payloads/UpdateCountryPayload.cs
+++ Geo.Contracts/Payloads/UpdateCountryPayload.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Geo.Domain;
 
 namespace Geo.Contracts.Payloads
 {
-    class UpdateCountryPayload
-    {
-    }
+    public record UpdateCountryPayload(Country Country);
 }
