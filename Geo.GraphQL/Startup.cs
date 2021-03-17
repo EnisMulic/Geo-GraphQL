040accc3fc4bac56fe8692024a22330525aa7697
Enis Mulić <enis.s.mulic@gmail.com>
2021-03-17 12:35:34 +0100

    Add cors

--- Geo.GraphQL/Startup.cs
+++ Geo.GraphQL/Startup.cs
@@ -30,6 +30,8 @@ namespace Geo.GraphQL
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+
             services.AddPooledDbContextFactory<GeoDbContext>(options =>
                 options.UseNpgsql(Configuration.GetConnectionString("GeoDatabase")));
 
